3a7f4ae7c3a3cffcf1c77db55fed764cb8968380
Daphne Staruch <daphstr@gmail.com>
2023-09-14 16:14:29 -0400

    cleaning code

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// You can remove this line when you're ready.
-console.log("Good luck!");
 const button = document.querySelector(".button");
 const success = document.querySelector(".success");
 const formDiv = document.querySelector(".form");
@@ -7,26 +5,18 @@ const formDiv = document.querySelector(".form");
 const form = document.querySelector("#userForm");
 
 form.addEventListener("submit", (event) => {
-  console.log(form);
-
-  // Prevent submit
   event.preventDefault();
-  // find inputs, textareas, and selects within the parent
   let children = event.target.querySelectorAll("input, checkbox, select");
   console.log(children);
-  // find if any of them are empty
   let findEmpty = Array.from(children).find((element) => {
     if (element.value.length < 1) {
       return true;
     }
     return false;
   });
-  // check if found an empty child
   if (findEmpty) {
-    // if so alert
     alert(findEmpty.name);
   } else {
-    // if not submit form
     formDiv.classList.add("d-none");
     success.classList.remove("d-none");
   }
