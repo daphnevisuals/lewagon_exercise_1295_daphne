ea80c87a9fcf16f6cffc0a801ad5808277441b8b
Daphne Staruch <daphstr@gmail.com>
2023-09-14 15:59:24 -0400

    cleaning code

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const button = document.querySelector(".button");
 const success = document.querySelector(".success");
 const formDiv = document.querySelector(".form");
 
-// button.addEventListener("click", (e) => {
-//   formDiv.classList.add("d-none");
-//   success.classList.remove("d-none");
-// });
-
 const form = document.querySelector("#userForm");
 
 form.addEventListener("submit", (event) => {
@@ -36,15 +31,3 @@ form.addEventListener("submit", (event) => {
     success.classList.remove("d-none");
   }
 });
-
-// const form = document.querySelector("#userForm");
-// form.addEventListener("submit", function (e) {
-//   if (form.checkValidity()) {
-//     button.addEventListener("click", (e) => {
-//       formDiv.classList.add("d-none");
-//       success.classList.remove("d-none");
-//     });
-//   }
-//   alert("you need to fill form");
-//   e.preventDefault();
-// });
