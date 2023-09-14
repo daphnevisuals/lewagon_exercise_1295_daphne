62f3b7b0c45b774525643b194a3be8009b53b3c4
Daphne Staruch <daphstr@gmail.com>
2023-09-14 15:57:14 -0400

    form validate inputss working

--- main.js
+++ main.js
@@ -4,19 +4,47 @@ const button = document.querySelector(".button");
 const success = document.querySelector(".success");
 const formDiv = document.querySelector(".form");
 
-button.addEventListener("click", (e) => {
-  formDiv.classList.add("d-none");
-  success.classList.remove("d-none");
-});
+// button.addEventListener("click", (e) => {
+//   formDiv.classList.add("d-none");
+//   success.classList.remove("d-none");
+// });
 
 const form = document.querySelector("#userForm");
-form.addEventListener("submit", function (e) {
-  if (form.checkValidity()) {
-    button.addEventListener("click", (e) => {
-      formDiv.classList.add("d-none");
-      success.classList.remove("d-none");
-    });
+
+form.addEventListener("submit", (event) => {
+  console.log(form);
+
+  // Prevent submit
+  event.preventDefault();
+  // find inputs, textareas, and selects within the parent
+  let children = event.target.querySelectorAll("input, checkbox, select");
+  console.log(children);
+  // find if any of them are empty
+  let findEmpty = Array.from(children).find((element) => {
+    if (element.value.length < 1) {
+      return true;
+    }
+    return false;
+  });
+  // check if found an empty child
+  if (findEmpty) {
+    // if so alert
+    alert(findEmpty.name);
+  } else {
+    // if not submit form
+    formDiv.classList.add("d-none");
+    success.classList.remove("d-none");
   }
-  alert("you need to fill form");
-  e.preventDefault();
 });
+
+// const form = document.querySelector("#userForm");
+// form.addEventListener("submit", function (e) {
+//   if (form.checkValidity()) {
+//     button.addEventListener("click", (e) => {
+//       formDiv.classList.add("d-none");
+//       success.classList.remove("d-none");
+//     });
+//   }
+//   alert("you need to fill form");
+//   e.preventDefault();
+// });
