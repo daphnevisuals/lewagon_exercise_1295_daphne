38193ecd4fe173fe7adfe4267c8c63aa8ca2f270
Daphne Staruch <daphstr@gmail.com>
2023-09-14 15:27:32 -0400

    added javascript

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // You can remove this line when you're ready.
-console.log('Good luck!');
+console.log("Good luck!");
+const button = document.querySelector(".button");
+const success = document.querySelector(".success");
+const form = document.querySelector(".form");
+
+button.addEventListener("click", (e) => {
+  form.classList.add("d-none");
+  success.classList.remove("d-none");
+});
